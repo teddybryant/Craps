933b9c72464a28ba18c2be737370c522e0cb72ca
Teddy Bryant <teddybryant88@gmail.com>
2020-01-15 08:58:01 -0800

    First commit

--- Die.java
+++ Die.java
@@ -1,33 +1,21 @@
 
 /**
- * Write a description of class Die here.
+ * Rolls a die and returns the value. 
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Teddy Bryant
+ * @version 2019-1-15
  */
 public class Die
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Die
-     */
-    public Die()
+    private int result;
+    public int roll()
     {
-        // initialise instance variables
-        x = 0;
+        result = ((int) (Math.random() * 6) + 1);
+        return result;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+    
+    public int getResult()
     {
-        // put your code here
-        return x + y;
+        return result;
     }
 }
